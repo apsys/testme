4c36a9a1af3e51232e0294605fee52b38c41ba6b
vn0gdpo <amit.panchabhai@walmartlabs.com>
2020-08-08 18:06:11 -0700

    fixed

--- src/main/java/com/amit/testme/ReverseNumber.java
+++ src/main/java/com/amit/testme/ReverseNumber.java
@@ -1,6 +1,16 @@
 package com.amit.testme;
 
 public class ReverseNumber {
+	
+	public static void rev(int n) {
+		int sum=0; int no=0;
+		while(n>0) {
+			no=n%10; //143%10=3, 14%10=4, 1
+			n=n/10; //143/10=14, 14/10=1, 0
+			sum = sum*10+no; //3, 3*10+4=34, 
+		}
+		System.out.println("reversed no: "+sum);
+	}
 
 
 	public static void main(String[] args) {
@@ -8,19 +18,6 @@ public class ReverseNumber {
 		rev(143);
 		rev(87320);
 		rev(1424);
-		
-		
-		
-		public static void rev(int n) {
-			int sum=0; int no=0;
-			while(n>0) {
-				no=n%10; //143%10=3, 14%10=4, 1
-				n=n/10; //143/10=14, 14/10=1, 0
-				sum = sum*10+no; //3, 3*10+4=34, 
-			}
-			System.out.println("reversed no: "+sum);
-		}
-
 	}
 
 }
